fded4a790243bb7aa7849ae6629fe1734d49f344
Im-29 <faimtarmizai04@gmail.com>
2026-08-02 17:41:22 +0800

    Update API endpoint to production URL

--- estimator.js
+++ estimator.js
@@ -60,7 +60,7 @@ form.addEventListener("submit", async function (e) {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://python-api-7xbz.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
