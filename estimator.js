3c3418b768c75f17b73f3103945bb0bc63358a38
Im-29 <faimtarmizai04@gmail.com>
2026-08-02 16:17:17 +0800

    Change fetch URL from HTTPS to HTTP

--- estimator.js
+++ estimator.js
@@ -60,7 +60,7 @@ form.addEventListener("submit", async function (e) {
     }
 
     try {
-      const res = await fetch("https://127.0.0.1:5000/predict", {
+      const res = await fetch("http://127.0.0.1:5000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
