57133601c18de5ab315aee1889e733f3dc48ece3
Im-29 <faimtarmizai04@gmail.com>
2026-08-02 16:50:19 +0800

    Fix URL for sending notification message

--- editusername.js
+++ editusername.js
@@ -126,7 +126,7 @@ form.addEventListener("submit", async function (e) {
       alert("Username successfully updated!");
 
       // send notification
-      await fetch("https://house-price-prediction-1943.onrender.com/send-messages", {
+      await fetch("https://house-price-prediction-1943.onrender.com/send-message", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
