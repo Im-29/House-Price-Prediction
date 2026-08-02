49d21015949fa3988a3dcc4dc29e213364e2e70a
Im-29 <faimtarmizai04@gmail.com>
2026-08-02 16:32:47 +0800

    Update register.js

--- register.js
+++ register.js
@@ -24,7 +24,7 @@ form.addEventListener("submit", async function(e) {
   }
 
   try {
-    const res = await fetch("http://localhost:3000/register", {
+    const res = await fetch("https://house-price-prediction-1943.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
